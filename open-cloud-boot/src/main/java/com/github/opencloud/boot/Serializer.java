c88bf031dbb75e902e7e302670db6ab97205aab6
marklin <2273990296@qq.com>
2022-10-01 00:37:38 +0800

    [2022-10-01 00:37 星期六]: Initial commit pom.xml file template to master

---
+++ open-cloud-boot/src/main/java/com/github/opencloud/boot/Serializer.java
@@ -0,0 +1,13 @@
+package com.github.opencloud.boot;
+
+import java.io.Serializable;
+
+/**
+ * <p>封装OpenCloud项目Serializer类.<br></p>
+ *
+ * @author Powered by marklin 2022-10-01 00:26
+ * @version 1.0.0
+ * <p>Copyright © 2018-2022 Pivotal Cloud Technology Systems Incorporated. All rights reserved.<br></p>
+ */
+public interface Serializer extends Serializable {
+}
